4d322aadd9500ac06f9d9f1cdf346391b62f4cac
EvrettG <skyrift@tpg.com.au>
2024-07-06 21:05:46 +1000

    api route files made, finnished the user model

--- models/thought.js
+++ models/thought.js
@@ -1,1 +1,3 @@
 // note thoughts and reactions will go here, with reactions being a sub document/schema of thoughts
+const { Schema, model } = require('mongoose');
+
---
+++ models/user.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+// Regular expression to validate email addresses
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const userSchema = new Schema({
+    user: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [emailRegex, 'Please enter a valid email address']
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'thought'
+      }],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'user'
+    }]
+  },
+{
+    toJSON: {
+        virtuals: true,
+      },
+    id: false,
+});
+
+// Add virtual for friendCount
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+  // Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
